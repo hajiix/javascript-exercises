46f063305c28468855949616392f3a43db7f83bd
Abdurkader Abdi <abdurkaderabdi@gmail.com>
2023-12-29 13:56:07 -0600

    Complete reverseString in js-exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let reversedStr = ""
+  for(i = 0; i < str.length; i++){
+    reversedStr = str[i] + reversedStr
+  }
+  return reversedStr
 };
 
 // Do not edit below this line
