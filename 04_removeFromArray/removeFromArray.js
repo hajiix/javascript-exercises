8317894328a0759827494a5175daed68b1fa3f62
Abdurkader Abdi <abdurkaderabdi@gmail.com>
2023-12-29 21:07:07 -0600

    Complete removeFromArray in js-exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+  for(i = 1; i < arguments.length; i++){
+    let removedIndex = arr.indexOf(arguments[i]);
+    if (removedIndex < 0) continue;
+    arr.splice(removedIndex, 1);
+  }
+  return arr;
 };
 
 // Do not edit below this line
